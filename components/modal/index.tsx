290eb19bdba2ee375a1903315423e05062b26b26
WRaquel <raquelwetzel@gmail.com>
2025-12-18 12:11:52 +0000

    fix bug not show backdrop modal

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -12,33 +12,20 @@ export interface ModalProps {
   footer?:ReactElement
 }
 const Modal = forwardRef<HTMLDialogElement, ModalProps>(({header, body, footer}, ref) => { 
-  const {dialogRef } = useModal();
-  const isOpen = dialogRef.current?.open
-
-  useEffect(()=>{
-    if(isOpen){
-      document.body.style.overflow = "hidden";
-    }
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, [isOpen])
 
   return (
     <>
-    {isOpen && (
       <div className={style.backdrop} aria-hidden="true"/>
-    )}
-    <div className={style.dialog}>
-      <dialog ref={ref}>
-        <div className={style.header}>
-          <h1>{header.text}</h1>
-          <span>{header.icon}</span>
-        </div>
-        <div className={style.body}>{body}</div>
-        {footer && <div className={style.footer}>{footer}</div>}
-      </dialog> 
-    </div>
+      <div className={style.dialog}>
+        <dialog ref={ref}>
+          <div className={style.header}>
+            <h1>{header.text}</h1>
+            <span>{header.icon}</span>
+          </div>
+          <div className={style.body}>{body}</div>
+          {footer && <div className={style.footer}>{footer}</div>}
+        </dialog> 
+      </div>
     </>
   );
 });
