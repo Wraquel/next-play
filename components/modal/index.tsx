63e78aa6f0dc475123df8e24569ad1bd76c043a7
WRaquel <raquelwetzel@gmail.com>
2026-01-12 16:46:45 +0000

    fix bug not show scroll when close modal, fix bug cursors inside modal

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,5 +1,5 @@
 import style from "./style/modal.module.scss"
-import { forwardRef, ReactElement, useEffect } from "react";
+import { forwardRef, ReactElement, use, useEffect, useId, useImperativeHandle, useRef} from "react";
 
 export interface HeaderProps {
   text?:string;
@@ -10,31 +10,30 @@ export interface ModalProps {
   body:ReactElement,
   footer?:ReactElement
 }
-const Modal = forwardRef<HTMLDialogElement, ModalProps>(({header, body, footer}, ref) => {
+const Modal = forwardRef<HTMLDialogElement | null, ModalProps>(({header, body, footer}, ref) => {
+  const _ref= useRef<HTMLDivElement | null>(null);
+  const dialogWrapper = useRef<HTMLDialogElement | null >(null);
+
+  useImperativeHandle(ref, () => dialogWrapper.current!);
 
   useEffect(() => {
-    const dialog = ref && 'current' in ref ? ref.current : null;
+    const dialog = dialogWrapper.current;
+    const headerRef = _ref.current;
     const body = document.body;
     if(dialog) body.style.position = 'relative';
-    body.style.overflow = 'hidden';
-
-    console.log("Body dimensions:", body.getBoundingClientRect());
 
     let dragging = false;
     let offsetX = 0;
     let offsetY = 0;
 
     const handleMouseDown = (event: MouseEvent) => {
-      if (dialog && event.target === dialog.querySelector(`#draggable-profile`)) {
+      if (dialog && event.target === headerRef) {
         dragging = true;
-        // console.log("DOWN", event);
         const rect = dialog.getBoundingClientRect();
-        // console.log("rect", rect);
 
         dialog.style.left = `${rect.left}px`;
         dialog.style.top = `${rect.top}px`;
 
-        dialog.style.cursor = 'grabbing';
         dialog.style.transform = 'none';
 
         offsetX = event.clientX - rect.left;
@@ -45,8 +44,7 @@ const Modal = forwardRef<HTMLDialogElement, ModalProps>(({header, body, footer},
 
     const handleMouseMove = (event: MouseEvent) => {
       if (dragging && dialog) {
-        // console.log("MOVE", event);
-        dialog.style.cursor = 'grabbing';
+        if (headerRef) headerRef.style.cursor = 'grabbing';
         let x = event.clientX - offsetX;
         let y = event.clientY - offsetY;
 
@@ -63,9 +61,7 @@ const Modal = forwardRef<HTMLDialogElement, ModalProps>(({header, body, footer},
 
     const handleMouseUp = () => {
       dragging = false;
-      if (dialog) {
-        dialog.style.cursor = 'grab';
-      }
+      if (headerRef) headerRef.style.cursor = 'move';
     };
 
     document.addEventListener('mousedown', handleMouseDown);
@@ -82,8 +78,8 @@ const Modal = forwardRef<HTMLDialogElement, ModalProps>(({header, body, footer},
     <>
       <div className={style.backdrop} aria-hidden="true"/>
       <div className={style.dialog}>
-        <dialog ref={ref}>
-          <div className={style.header} id="draggable-profile">
+        <dialog ref={dialogWrapper}>
+          <div className={style.header} ref={_ref}>
             <h1>{header.text}</h1>
             <span>{header.icon}</span>
           </div>
